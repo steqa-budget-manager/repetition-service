ff3828eff9cd468711d364ac8111ff140b60a3e6
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2025-04-27 20:28:22 +0700

    feat: Init

---
+++ src/main/java/ru/steqa/repetitionservice/RepetitionServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.steqa.repetitionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepetitionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepetitionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/steqa/repetitionservice/RepetitionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.steqa.repetitionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepetitionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
